2a0a8f0e953cfbfc1303dd0a009cbcba8a82892b
kali19 <khadijah.syed.ali@gmail.com>
2023-04-10 17:56:16 -0400

    timer added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,27 @@
 document.getElementById("start_quiz_button").addEventListener("click", question1);
 document.getElementById("question1").addEventListener("click", question2);
 
-document.getElementById("q1wrong").addEventListener("click", q1wrong_handler);
+var q1wrong_answers = document.getElementsByClassName("q1wrong");
+for (let item of q1wrong_answers) {
+    item.addEventListener("click", q1wrong_handler);
+}
+
+var q2wrong_answers = document.getElementsByClassName("q2wrong");
+for (let item of q2wrong_answers) {
+    item.addEventListener("click", q2wrong_handler);
+}
+
+var q3wrong_answers = document.getElementsByClassName("q3wrong");
+for (let item of q3wrong_answers) {
+    item.addEventListener("click", q3wrong_handler);
+}
+
 document.getElementById("q1right").addEventListener("click", q1right_handler);
 
 document.getElementById("question2").addEventListener("click", question3);
-document.getElementById("q2wrong").addEventListener("click", q2wrong_handler);
 document.getElementById("q2right").addEventListener("click", q2right_handler);
 
 document.getElementById("question3").addEventListener("click", results);
-document.getElementById("q3wrong").addEventListener("click", q3wrong_handler);
 document.getElementById("q3right").addEventListener("click", q3right_handler);
 
 function clear_instructions() {
@@ -25,48 +37,88 @@ function clear_instructions() {
 
 function question1() {
     clear_instructions();
-    document.getElementById("question1").style= 'display:inline';
+    document.getElementById("question1").style = 'display:inline';
+
 }
 
 function q1right_handler() {
-    document.getElementById("question1").style= 'display:none';
-    document.getElementById("current_results").innerHTML= 'RIGHT!';
+    document.getElementById("question1").style = 'display:none';
+    document.getElementById("current_results").innerHTML = 'RIGHT!';
+    document.getElementById("current_results").fadeOut();
+    $("#current_results").fadeOut();
 }
 
 function q1wrong_handler() {
-    document.getElementById("question1").style= 'display:none';    
-    document.getElementById("current_results").innerHTML= 'WRONG!';
+    document.getElementById("question1").style = 'display:none';
+    score = score -15;
+    document.getElementById("current_results").innerHTML = 'WRONG!';
+    $("#current_results").fadeOut();
 }
 
 function question2() {
     clear_instructions();
-    document.getElementById("question2").style= 'display:inline';
+    document.getElementById("question2").style = 'display:inline';
 }
 
 function q2right_handler() {
-    document.getElementById("question2").style= 'display:none';
-    document.getElementById("current_results").innerHTML= 'RIGHT!';
+    document.getElementById("question2").style = 'display:none';
+    document.getElementById("current_results").innerHTML = 'RIGHT!';
+    $("#current_results").fadeIn();
+    $("#current_results").fadeOut();
 }
 
 function q2wrong_handler() {
-    document.getElementById("question2").style= 'display:none';    
-    document.getElementById("current_results").innerHTML= 'WRONG!';
+    document.getElementById("question2").style = 'display:none';
+    document.getElementById("current_results").innerHTML = 'WRONG!';
+    score = score -15;
+    $("#current_results").fadeIn();
+    $("#current_results").fadeOut();
 }
 
 function question3() {
     clear_instructions();
-    document.getElementById("question3").style= 'display:inline';
+    document.getElementById("question3").style = 'display:inline';
 }
 
 function q3right_handler() {
-    document.getElementById("question3").style= 'display:none';
-    document.getElementById("current_results").innerHTML= 'RIGHT!';
+    document.getElementById("question3").style = 'display:none';
+    document.getElementById("current_results").innerHTML = 'RIGHT!';
+    $("#current_results").fadeIn();
+    $("#current_results").fadeOut();
 }
 
 function q3wrong_handler() {
-    document.getElementById("question3").style= 'display:none';    
-    document.getElementById("current_results").innerHTML= 'WRONG!';
+    document.getElementById("question3").style = 'display:none';
+    document.getElementById("current_results").innerHTML = 'WRONG!';
+    score = score -15;
+    $("#current_results").fadeIn();
+    $("#current_results").fadeOut();
 }
+
+function results() {
+    document.getElementById("results").style = 'display:inline';
+    document.getElementById("question3").style = 'display:none';
+    document.getElementById("question2").style = 'display:none';
+    document.getElementById("question1").style = 'display:none';
+    //    document.getElementById("current_results").innerHTML= 'All done';
+}
+
+let score = 60;
+let timerVar = '';
+function startTimer() {
+  timerVar = setInterval(myFunction, 1000);
+}
+function myFunction() {
+  score = score - 1;
+  document.getElementById("showscore").innerHTML = score;
+  if (score == 0) {
+    clearInterval(timerVar);
+    results();
+  }
+}
+
+
+
 
 // function question2() {
 //     clear_instructions();
