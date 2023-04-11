e5f1245f18783c831e752973cb47c99deb5025ea
kali19 <khadijah.syed.ali@gmail.com>
2023-04-11 18:56:43 -0400

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,19 +27,10 @@ document.getElementById("q3right").addEventListener("click", q3right_handler);
 
 function loadTitle() {
     window.location.reload();
-   // document.getElementById("instructions_block").style = 'display:inline';
-   // document.getElementById("highscores").style = 'display:none';
 }
 
 function clear_instructions() {
     document.getElementById("instructions_block").style = 'display:none';
-    
-    // var node1 = document.getElementById('instructions1');
-    // var node2 = document.getElementById('instructions2');
-    //  var node3 = document.getElementById('start_quiz_button');
-    // node1.style.visibility = 'hidden';
-    // node2.style.visibility = 'hidden';
-    // node3.style.visibility = 'hidden';
 }
 
 function question1() {
@@ -119,8 +110,6 @@ function results() {
     document.getElementById("showscore").style = 'display:none';
 
     document.getElementById("submit").addEventListener("click", setscore);
-
-    // document.getElementById("").innerHTML = localStorage.getItem("initials", "hs");
     
     document.getElementById("gb").addEventListener("click", loadTitle);
     
@@ -156,11 +145,6 @@ function highscores() {
         $('#highscores').append("<br>");
 
     }
-    // document.getElementById("question3").style = 'display:none';
-    // document.getElementById("question2").style = 'display:none';
-    // document.getElementById("question1").style = 'display:none';
-    // document.getElementById("results").style = 'display:none';
-    //    document.getElementById("current_results").innerHTML= 'All done';
 }
 function clearhs() {
     localStorage.clear();
@@ -169,48 +153,3 @@ function clearhs() {
 document.getElementById("ch").addEventListener("click", clearhs);
 
 
-// function question2() {
-//     clear_instructions();
-//     document.getElementById("question_paragraph").innerHTML = 
-//     "<h1>Question 2</h1><button id='question3'>Next</button>";
-// }
-
-
-// function setup() {
-//     //var q1 = document.getElementById('question1');
-//     // var q2 = document.getElementById('question2');
-//     // var q3 = document.getElementById('question3');
-//     // var q4 = document.getElementById('question4');
-//     //q1.style.visibility = 'hidden';
-//     // q2.style.visibility = 'hidden';
-//     // q3.style.visibility = 'hidden';
-//     // q4.style.visibility = 'hidden';
-// }
-
-// function startQuiz() {
-//     var node1 = document.getElementById('instructions1');
-//     var node2 = document.getElementById('instructions2');
-//     var node3 = document.getElementById('instructions_button');
-//     var node4 = document.getElementById('question1');
-//     var instructionsnode = document.getElementById('instructionsblock');
-
-
-//     if (node1.style.visibility == 'visible') {
-//         instructionsnode.style.visibility = 'hidden';
-//         // node1.style.visibility = 'hidden';
-//         // node2.style.visibility = 'hidden';
-//         // node3.style.visibility = 'hidden';
-//         // node1.style.display = 'none';
-//         // node2.style.display = 'none';
-//         // node3.style.display = 'none';
-//         node4.hidden = false;
-
-//     }
-//     else {
-//         instructionsnode.style.visibility = 'visible'
-//         // node1.style.visibility = 'visible'
-//         // node2.style.visibility = 'visible'
-//         // node3.style.visibility = 'visible'
-//         node4.hidden = true;
-//     }
-// }
